8e83a86c5cc2c46be01a5406ed434f0e025b1881
mwfarb <mwfarb@andrew.cmu.edu>
2021-06-23 17:19:50 -0400

    add auth flow inspector updates

--- SyncTest/Assets/ArenaClient.cs
+++ SyncTest/Assets/ArenaClient.cs
@@ -1,20 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
+﻿using System;
 using System.IO;
 using System.Threading;
-using System.Threading.Tasks;
-
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Oauth2.v2;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
-using System.Linq;
-using System.Text;
+using UnityEngine;
 
+[HelpURL("https://arena.conix.io")]
 public class ArenaClient : MonoBehaviour
 {
+    [Header("ARENA Configuration")]
+    [Tooltip("IP address or URL of the host running the broker")]
+    public string host = "arenaxr.org";
+    [Tooltip("Name of the scene, without namespace ('example', not 'username/example'")]
+    public string sceneName = "example";
+    [Tooltip("Authorization code from web authentication.")]
+    public string authCode = null;
+    [Tooltip("Browser URL for the scene.")]
+    [TextArea(minLines: 1, maxLines: 2)]
+    public string sceneUrl = null;
+
+
+    [Header("Optional Parameters")]
+    [Tooltip("Namespace (automated with username), but can be overridden")]
+    public string namespaceName = null;
+    [Tooltip("UserName for the MQTT broker. Keep blank if no user name is required.")]
+    public string mqttUserName = null;
+    [Tooltip("Password for the MQTT broker. Keep blank if no password is required.")]
+    public string mqttPassword = null;
+
+
+    public string gAuthId = null;
+    public string gAuthEmail = null;
+
+
     static string[] Scopes = {
         Oauth2Service.Scope.UserinfoProfile,
         Oauth2Service.Scope.UserinfoEmail,
@@ -31,9 +51,10 @@ public class ArenaClient : MonoBehaviour
             //      System.Environment.SpecialFolder.Personal);
             // credPath = Path.Combine(credPath, ".arena_google_auth");
             string credPath = "token.json";
+            string applicationName = "ArenaClientCSharp";
 
             credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
+                    GoogleClientSecrets.FromStream(stream).Secrets,
                     Scopes,
                     "user",
                     CancellationToken.None,
@@ -43,11 +64,14 @@ public class ArenaClient : MonoBehaviour
             var oauthService = new Oauth2Service(new BaseClientService.Initializer()
             {
                 HttpClientInitializer = credential,
-                ApplicationName = "ArenaClientCSharp",
+                ApplicationName = applicationName,
             });
 
             var userInfo = oauthService.Userinfo.Get().Execute();
             Console.WriteLine("userInfo: " + userInfo);
+
+            gAuthEmail = userInfo.Email;
+            gAuthId = userInfo.Id;
         }
 
     }
@@ -55,6 +79,6 @@ public class ArenaClient : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        sceneUrl = "https://arena-dev1.conix.io/mwfarb/test";
     }
 }
