9267973713c76eba44a6bcc0692dd225e68d449d
mwfarb <mwfarb@andrew.cmu.edu>
2021-11-24 09:51:05 -0500

    add namespace

--- SyncTest/Assets/CoroutineWithData.cs
+++ SyncTest/Assets/CoroutineWithData.cs
@@ -3,24 +3,24 @@ using UnityEngine;
 
 namespace ArenaUnity
 {
-public class CoroutineWithData
-{
-    public Coroutine coroutine { get; private set; }
-    public object result;
-    private IEnumerator target;
-    public CoroutineWithData(MonoBehaviour owner, IEnumerator target)
+    public class CoroutineWithData
     {
-        this.target = target;
-        this.coroutine = owner.StartCoroutine(Run());
-    }
-
-    private IEnumerator Run()
-    {
-        while (target.MoveNext())
+        public Coroutine coroutine { get; private set; }
+        public object result;
+        private IEnumerator target;
+        public CoroutineWithData(MonoBehaviour owner, IEnumerator target)
         {
-            result = target.Current;
-            yield return result;
+            this.target = target;
+            this.coroutine = owner.StartCoroutine(Run());
+        }
+
+        private IEnumerator Run()
+        {
+            while (target.MoveNext())
+            {
+                result = target.Current;
+                yield return result;
+            }
         }
     }
-}
 }
