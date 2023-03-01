e6f379d5d5c74998da383d3614758f0b82ba1273
mwfarb <mwfarb@andrew.cmu.edu>
2023-03-01 00:30:51 -0500

    laser update

--- Samples/LaserPointer/Assets/LaserPointer.cs
+++ Samples/LaserPointer/Assets/LaserPointer.cs
@@ -11,16 +11,6 @@ public class LaserPointer : MonoBehaviour
     private Color32 _laserColor = new Color32(255, 0, 0, 255);
     private Vector3 _targetScale = new Vector3(.06f, .06f, .06f);
 
-    void Awake()
-    {
-        //_scene = ArenaClientScene.Instance;
-        //if (!_scene) gameObject.AddComponent<ArenaClientScene>();
-        //_scene.hostAddress = "arena-dev1.conix.io";
-        //_scene.namespaceName = "mwfarb";
-        //_scene.sceneName = "example5";
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(RunProgram());
@@ -29,49 +19,32 @@ public class LaserPointer : MonoBehaviour
     IEnumerator RunProgram()
     {
         _scene = ArenaClientScene.Instance;
-        //_scene.ConnectArena();
-        //yield return new WaitUntil(() => _scene.mqttClientConnected);
-        //yield return new WaitUntil(() => _scene.persistLoaded);
-        //if (_scene.arenaObjs == null) yield break;
-        //yield return new WaitUntil(() => _scene.arenaObjs.Count >= 3);
-        yield return new WaitForSeconds(5);
+        yield return new WaitUntil(() => _scene.persistLoaded);
 
         // find clickable arena objects
-
-        // TODO: find balance of ACL attached complete, or in progress
-
         foreach (var aobj in _scene.arenaObjs.Values)
         {
             // attach callbacks
             ArenaClickListener acl = aobj.GetComponent<ArenaClickListener>();
-            if (acl)
+            if (acl != null)
             {
-                acl.OnEventCallback = MyMouseDown;
-                Debug.Log($"Click callback attached to {aobj.name}");
+                acl.OnEventCallback = MouseEventCallback;
+                Debug.Log($"Laser: mouse callback attached to {aobj.name}");
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if (!_scene) _scene = ArenaClientScene.Instance;
     }
 
     /// <summary>
     /// A delegate method used as a callback to go some special handling on incoming messages.
     /// </summary>
-    private void MyMouseDown(string event_type, dynamic data)
+    private void MouseEventCallback(string event_type, dynamic data)
     {
-        Debug.Log($"Remote Click '{JsonComvert.Deserialize(data)}' ({event_type})!");
-
         if (event_type != "mousedown") return;
         int instance = UnityEngine.Random.Range(0, 100000000);
 
-        //laser
-        string line_id = $"line-{instance}";
+        // laser
         dynamic msg = new ExpandoObject();
-        msg.object_id = line_id;
+        msg.object_id = $"line-{instance}";
         msg.action = "create";
         msg.type = "object";
         msg.ttl = 1;
@@ -84,10 +57,9 @@ public class LaserPointer : MonoBehaviour
         msg.data.lineWidth = 5;
         string payload = JsonConvert.SerializeObject(msg);
         _scene.PublishObject(msg.object_id, payload);
-        //ball
-        string target_id = $"target-{instance}";
+        // target
         msg = new ExpandoObject();
-        msg.object_id = target_id;
+        msg.object_id = $"target-{instance}"; ;
         msg.action = "create";
         msg.type = "object";
         msg.ttl = 1;
