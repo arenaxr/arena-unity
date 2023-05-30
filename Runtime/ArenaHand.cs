01834bc2ca41fc26983f17d0dda0e50066ee71f6
mwfarb <mwfarb@andrew.cmu.edu>
2023-05-30 02:19:06 -0400

    improve visibility and notes

--- Runtime/ArenaHand.cs
+++ Runtime/ArenaHand.cs
@@ -7,6 +7,11 @@ using UnityEngine;
 
 namespace ArenaUnity
 {
+    /// <summary>
+    /// Temporary manager of drawing remote rays from hand controllers
+    /// TODO (mwfarb): resolve oculus-touch controls publishing +43 x-axis rotation orientationOffset from arena-web
+    /// TODO (mwfarb): set a proper arena-hand manager when we integrate with unity vr helmets
+    /// </summary>
     [DisallowMultipleComponent]
     [RequireComponent(typeof(ArenaObject))]
     public class ArenaHand : MonoBehaviour
@@ -18,7 +23,7 @@ namespace ArenaUnity
         private void Start()
         {
             start = Vector3.zero;
-            end = Vector3.forward * 100f;  // use back vector to simulate gltf rotation matrix transform
+            end = Vector3.forward * 1000f;
             color = Color.white;
 
             GameObject rayObj = new GameObject($"ray_{name}");
@@ -27,8 +32,12 @@ namespace ArenaUnity
             Vector3[] nodes = { start, end };
             line.SetPositions(nodes);
             line.startColor = line.endColor = color;
+            //line.material = new Material(Shader.Find("Standard"));
+            line.material = new Material(Shader.Find("Unlit/Color"));
+            //line.material = new Material(Shader.Find("Default-Line"));
+            //line.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
             line.widthMultiplier = 1f * ArenaUnity.LineSinglePixelInMeters;
-
+            // we also need to apply the gltf rotation matrix transform, since the hand models did as well
             rayObj.transform.localRotation = ArenaUnity.GltfToUnityRotationQuat(rayObj.transform.localRotation);
             // makes the child keep its local orientation rather than its global orientation
             rayObj.transform.SetParent(transform, false);
