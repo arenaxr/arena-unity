7bfc1d8368aa62750be2a3d2e01e61998d2d0f9c
mwfarb <mwfarb@andrew.cmu.edu>
2025-08-18 16:30:16 -0400

    chore: sync with latest phyx schema

--- Runtime/Schemas/ArenaAnimationJson.cs
+++ Runtime/Schemas/ArenaAnimationJson.cs
@@ -169,6 +169,7 @@ namespace ArenaUnity.Schemas
         }
 
         private static string defFrom = "";
+        [JsonConverter(typeof(string))] // TODO (mwfarb): remove explicit conversion to avoid walking errors
         [JsonProperty(PropertyName = "from")]
         [Tooltip("Initial value at start of animation. If not specified, the current property value of the entity will be used (will be sampled on each animation start). It is best to specify a from value when possible for stability.")]
         public string From = defFrom;
@@ -249,6 +250,7 @@ namespace ArenaUnity.Schemas
         }
 
         private static string defTo = "";
+        [JsonConverter(typeof(string))] // TODO (mwfarb): remove explicit conversion to avoid walking errors
         [JsonProperty(PropertyName = "to")]
         [Tooltip("Target value at end of animation.")]
         public string To = defTo;
