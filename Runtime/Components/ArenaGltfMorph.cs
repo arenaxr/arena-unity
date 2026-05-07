febf978c63c9eec1ec0102888e97491b30ec8be6
mwfarb <mwfarb@andrew.cmu.edu>
2026-05-07 15:01:30 -0400

    feat(gltf-morph): support gtlf features morph blending

--- Runtime/Components/ArenaGltfMorph.cs
+++ Runtime/Components/ArenaGltfMorph.cs
@@ -14,14 +14,42 @@ namespace ArenaUnity.Components
     public class ArenaGltfMorph : ArenaComponent
     {
         // ARENA gltf-morph component unity conversion status:
-        // TODO: morphtarget
-        // TODO: value
+        // DONE: morphtarget
+        // DONE: value
 
         public ArenaGltfMorphJson json = new ArenaGltfMorphJson();
 
+        private bool morphApplied = false;
+
         protected override void ApplyRender()
         {
-            // TODO: Implement this component if needed, or note our reasons for not rendering or controlling here.
+            morphApplied = false;
+        }
+
+        private new void Update()
+        {
+            if (!morphApplied)
+            {
+                var gltfModel = GetComponent<ArenaWireGltfModel>();
+                if (gltfModel == null || gltfModel.isLoaded)
+                {
+                    if (!string.IsNullOrEmpty(json.Morphtarget))
+                    {
+                        foreach (var smr in GetComponentsInChildren<SkinnedMeshRenderer>())
+                        {
+                            if (smr.sharedMesh != null)
+                            {
+                                int index = smr.sharedMesh.GetBlendShapeIndex(json.Morphtarget);
+                                if (index >= 0)
+                                {
+                                    smr.SetBlendShapeWeight(index, json.Value * 100f);
+                                }
+                            }
+                        }
+                    }
+                    morphApplied = true;
+                }
+            }
         }
 
         public override void UpdateObject()
