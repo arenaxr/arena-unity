7b3db0795f5abd6294c008aac388206f1fc18b24
mwfarb <mwfarb@andrew.cmu.edu>
2026-05-07 15:26:50 -0400

    feat(renderer-settings): support renderer conditions where possible

--- Runtime/Components/ArenaSceneRendererSettings.cs
+++ Runtime/Components/ArenaSceneRendererSettings.cs
@@ -14,16 +14,49 @@ namespace ArenaUnity.Components
     public class ArenaSceneRendererSettings: ArenaComponent
     {
         // ARENA renderer-settings component unity conversion status:
-        // TODO: localClippingEnabled
-        // TODO: outputColorSpace
-        // TODO: physicallyCorrectLights
-        // TODO: sortObjects
+        // DONE: localClippingEnabled
+        // DONE: outputColorSpace
+        // DONE: physicallyCorrectLights
+        // DONE: sortObjects
 
         public ArenaRendererSettingsJson json = new ArenaRendererSettingsJson();
 
         protected override void ApplyRender()
         {
-            // TODO: Implement this component if needed, or note our reasons for not rendering or controlling here.
+            if (Camera.main != null)
+            {
+                if (json.SortObjects)
+                {
+                    Camera.main.opaqueSortMode = UnityEngine.Rendering.OpaqueSortMode.Default;
+                    Camera.main.transparencySortMode = TransparencySortMode.Default;
+                }
+                else
+                {
+                    Camera.main.opaqueSortMode = UnityEngine.Rendering.OpaqueSortMode.NoDistanceSort;
+                    Camera.main.transparencySortMode = TransparencySortMode.CustomAxis;
+                    Camera.main.transparencySortAxis = Vector3.up; // arbitrary axis to disrupt standard sorting
+                }
+            }
+
+            // outputColorSpace mapping warning
+            bool isLinearUnity = QualitySettings.activeColorSpace == ColorSpace.Linear;
+            bool isLinearArena = json.OutputColorSpace == ArenaRendererSettingsJson.OutputColorSpaceType.LinearSRGBColorSpace;
+            if (isLinearUnity != isLinearArena && json.OutputColorSpace != ArenaRendererSettingsJson.OutputColorSpaceType.NoColorSpace)
+            {
+                Debug.LogWarning($"[ArenaSceneRendererSettings] ARENA requests outputColorSpace '{json.OutputColorSpace}' but Unity project is baked with '{QualitySettings.activeColorSpace}'. Cannot change at runtime.");
+            }
+
+            // localClippingEnabled warning
+            if (json.LocalClippingEnabled)
+            {
+                Debug.LogWarning("[ArenaSceneRendererSettings] localClippingEnabled is true, but Unity lacks native global clipping plane support without custom shaders.");
+            }
+
+            // physicallyCorrectLights warning
+            if (json.PhysicallyCorrectLights != UnityEngine.Rendering.GraphicsSettings.lightsUseLinearIntensity)
+            {
+                Debug.LogWarning($"[ArenaSceneRendererSettings] physicallyCorrectLights is {json.PhysicallyCorrectLights}, but Unity GraphicsSettings.lightsUseLinearIntensity is {UnityEngine.Rendering.GraphicsSettings.lightsUseLinearIntensity}. Cannot change at runtime.");
+            }
         }
 
         public override void UpdateObject()
