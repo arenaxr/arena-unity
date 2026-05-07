7dc952a96d59a2240c7d7277549a0741f1c268b8
mwfarb <mwfarb@andrew.cmu.edu>
2026-05-07 15:05:01 -0400

    feat(gltf-model-lod): support model load on demand

--- Runtime/Components/ArenaGltfModelLod.cs
+++ Runtime/Components/ArenaGltfModelLod.cs
@@ -14,16 +14,98 @@ namespace ArenaUnity.Components
     public class ArenaGltfModelLod : ArenaComponent
     {
         // ARENA gltf-model-lod component unity conversion status:
-        // TODO: detailedUrl
-        // TODO: detailedDistance
-        // TODO: updateRate
-        // TODO: retainCache
+        // DONE: detailedUrl
+        // DONE: detailedDistance
+        // DONE: updateRate
+        // DONE: retainCache
 
         public ArenaGltfModelLodJson json = new ArenaGltfModelLodJson();
 
+        private GameObject detailedModelObj;
+        private Coroutine lodCoroutine;
+        private bool isDetailedLoaded = false;
+
         protected override void ApplyRender()
         {
-            // TODO: Implement this component if needed, or note our reasons for not rendering or controlling here.
+            if (string.IsNullOrEmpty(json.DetailedUrl)) return;
+
+            if (ArenaClientScene.Instance != null)
+            {
+                string assetPath = ArenaClientScene.Instance.checkLocalAsset(json.DetailedUrl);
+                if (assetPath != null)
+                {
+                    if (detailedModelObj == null)
+                    {
+                        detailedModelObj = new GameObject("DetailedModelLOD");
+                        detailedModelObj.transform.SetParent(gameObject.transform, false);
+                        // Start hidden
+                        detailedModelObj.SetActive(false);
+                        ArenaWireGltfModel.AttachGltf(assetPath, detailedModelObj);
+                        isDetailedLoaded = true;
+                    }
+
+                    if (lodCoroutine != null) StopCoroutine(lodCoroutine);
+                    if (gameObject.activeInHierarchy)
+                        lodCoroutine = StartCoroutine(CheckDistanceRoutine());
+                }
+                else
+                {
+                    ArenaClientScene.Instance.RegisterAssetCallback(json.DetailedUrl, () => { apply = true; });
+                }
+            }
+        }
+
+        private System.Collections.IEnumerator CheckDistanceRoutine()
+        {
+            while (true)
+            {
+                if (Camera.main != null && isDetailedLoaded)
+                {
+                    float dist = Vector3.Distance(Camera.main.transform.position, transform.position);
+                    bool useDetailed = dist <= json.DetailedDistance;
+                    
+                    if (detailedModelObj != null && detailedModelObj.activeSelf != useDetailed)
+                    {
+                        detailedModelObj.SetActive(useDetailed);
+                        
+                        // Toggle base model(s) by disabling any child that isn't the detailed model
+                        // and isn't another ARENA object.
+                        foreach (Transform child in transform)
+                        {
+                            if (child.gameObject != detailedModelObj && child.GetComponent<ArenaObject>() == null)
+                            {
+                                child.gameObject.SetActive(!useDetailed);
+                            }
+                        }
+                    }
+                }
+                float waitTime = json.UpdateRate / 1000f;
+                if (waitTime <= 0) waitTime = 0.333f;
+                yield return new WaitForSeconds(waitTime);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (lodCoroutine != null)
+            {
+                StopCoroutine(lodCoroutine);
+                lodCoroutine = null;
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (isDetailedLoaded)
+            {
+                if (lodCoroutine != null) StopCoroutine(lodCoroutine);
+                lodCoroutine = StartCoroutine(CheckDistanceRoutine());
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (lodCoroutine != null) StopCoroutine(lodCoroutine);
         }
 
         public override void UpdateObject()
